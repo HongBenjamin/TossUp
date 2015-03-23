64b878a21693c19235c54e449c567485bf914065
Benjamin Hong <benjamin9081@gmail.com>
2015-03-23 09:07:51 -0500

    Benford Problem v1.0

---
+++ src/tossup/TossUp.java
@@ -0,0 +1,20 @@
+package tossup;
+
+import java.util.Scanner;
+
+public class TossUp 
+{
+    public static void main(String[] args) 
+    {
+        Scanner kbReader = new Scanner(System.in);
+        System.out.println("How many players are playing today?");
+        int players = kbReader.nextInt();
+        System.out.println("Enter your names.");
+        
+        for(int a = 0; a < players; a++)
+        {
+            String name = kbReader.nextLine();
+            Player
+        }
+    }    
+}
