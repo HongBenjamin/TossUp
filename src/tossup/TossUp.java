d6546548b469c512fa9835eee9ae8cc6fe09edbc
Benjamin Hong <benjamin9081@gmail.com>
2015-03-23 10:27:46 -0500

    Halfway done with playGame()

--- src/tossup/TossUp.java
+++ src/tossup/TossUp.java
@@ -1,21 +1,63 @@
 package tossup;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class TossUp 
 {
     static Scanner kbReader = new Scanner(System.in);
+    static ArrayList<Player> list = new ArrayList<>();
+    static int[] points;
+    static int players;
+    static boolean finished = false;
+    
     public static void main(String[] args) 
     {
-        System.out.println("How many players are playing today?");
-        int players = kbReader.nextInt();
-        System.out.println("Enter your names.");
+        System.out.println("Welcome to Toss Up, an obscure dice game now available to play on any java supported devices!"
+                + "\nHow many players are playing today? This game supports between 2-6 players.");
+        players = kbReader.nextInt();
         
+        if(players <2 || players > 6)
+        {
+            System.out.println("Didn't you read? This game only supports between 2-6 players!"
+            +"\nThe game will now terminate. Please try again.");
+            finished = true;
+        }
+        else
+        {
+            playGame();
+            endGame();
+        }        
+    }
+    
+    private static void playGame()
+    {                
         createPlayer(players);
+        
+        while(!finished)
+        {
+            for(int a = 0; a < players; a++)
+            {
+                Player temp1 = list.get(a);
+                boolean turnFinished = false;
+                while(!turnFinished)
+                {
+                    System.out.println("Hey " temp1.getName() + "! It's your turn! What would you like to do?")
+                    System.out.println("[Roll, End Turn]");
+                    String command = kbReader.nextLine;
+                    
+                }
+            }
+        }
+    }
+    
+    private static void endGame()
+    {
+        
     }
     
     private static void createPlayer(int x)
     {
+        System.out.println("Please enter your names.");
         if(x == 2)
         {
             System.out.println("What is Player 1's name?");
@@ -25,6 +67,9 @@ public class TossUp
             System.out.println("What is Player 2's name?");
             String name2 = kbReader.nextLine();
             Player p2 = new Player(name2);
+            
+            list.add(p1);
+            list.add(p2);
         }
         else if(x == 3)
         {
@@ -39,6 +84,10 @@ public class TossUp
             System.out.println("What is Player 3's name?");
             String name3 = kbReader.nextLine();
             Player p3 = new Player(name3);
+            
+            list.add(p1);
+            list.add(p2);
+            list.add(p3);
         }
         else if(x == 4)
         {
@@ -57,6 +106,11 @@ public class TossUp
             System.out.println("What is Player 4's name?");
             String name4 = kbReader.nextLine();
             Player p4 = new Player(name4);
+            
+            list.add(p1);
+            list.add(p2);
+            list.add(p3);
+            list.add(p4);
         }
         else if(x == 5)
         {
@@ -79,6 +133,12 @@ public class TossUp
             System.out.println("What is Player 5's name?");
             String name5 = kbReader.nextLine();
             Player p5 = new Player(name5);
+            
+            list.add(p1);
+            list.add(p2);
+            list.add(p3);
+            list.add(p4);
+            list.add(p5);
         }
         else if(x == 6)
         {
@@ -105,6 +165,13 @@ public class TossUp
             System.out.println("What is Player 6's name?");
             String name6 = kbReader.nextLine();
             Player p6 = new Player(name6);
+            
+            list.add(p1);
+            list.add(p2);
+            list.add(p3);
+            list.add(p4);
+            list.add(p5);
+            list.add(p6);
         }
     }
 }
