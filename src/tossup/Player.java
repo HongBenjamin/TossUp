1ca7f05e5436feb50a2fd6888673ef4c52da6706
Benjamin Hong <benjamin9081@gmail.com>
2015-03-26 21:11:52 -0500

    The Player class has had more added to it. Now returns total points, points accumulated in a turn, and the points that the player just rolled. Also helps identify when the game should end the turn for the player, or when to tell the player they did not score any points.

--- src/tossup/Player.java
+++ src/tossup/Player.java
@@ -4,11 +4,15 @@ public class Player extends Dice
 {
     String name;
     int currentPoints;
+    public int turn;
+    int totalPoints;
     
     public Player(String name)
     {
         this.name = name;
         currentPoints = 0;
+        turn = 0;
+        totalPoints = 0;
     }
     
     public String getName()
@@ -22,18 +26,50 @@ public class Player extends Dice
         addPoints();
     }
     
-    private int getPoints()
+    private void addPoints()//adds points
     {
-        return currentPoints;
+        totalPoints = totalPoints + rollPoint;
+        turn = turn + rollPoint;
     }
     
-    private boolean loseGame()
+    public void subtractPoints()//reverts the total points back to the value it was before the turn
+    {
+        totalPoints = totalPoints - turn;
+        turn = 0;
+    }
+    
+    public int getRollPoints()//returns the points rolled this roll
+    {
+        return rollPoint;
+    }
+    
+    public int getTurn()//returns the points accumulated this turn
+    {
+        return turn;
+    }
+    
+    public void resetTurn()//resets the turn
+    {
+        turn = 0;
+    }
+        
+    public int total()//returns total
+    {
+        return totalPoints;
+    }
+    
+    private boolean loseGame()//this does nothing right now
     {
         return false;
     }
     
-    public boolean endTurn()
+    public boolean endTurn()//end the turn
     {
-        return true;
+        return !keepGoing;
+    }
+    
+    public boolean almostEndTurn()//almost ends the turn
+    {
+        return warning;
     }
 }
